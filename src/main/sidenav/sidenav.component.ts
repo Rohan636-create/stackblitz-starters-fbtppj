d592c7a5dc5bc88b0bece12a78bef32d1ed891ea
Rohan636-create <141109390+Rohan636-create@users.noreply.github.com>
2023-08-07 13:11:37 +0530

    2 files modified, 7 added

--- src/main/sidenav/sidenav.component.ts
+++ src/main/sidenav/sidenav.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { ChatsComponent } from './chats/chats.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchComponent } from './search/search.component';
 
 @Component({
   standalone:true,
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
-  styleUrls: ['./sidenav.component.css']
+  styleUrls: ['./sidenav.component.css'],
+  imports:[HeaderComponent,SearchComponent,ChatsComponent],
 })
 export class SidenavComponent implements OnInit {
 
